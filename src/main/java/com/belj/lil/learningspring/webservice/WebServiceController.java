c7e68538be1a1add0f2448dbdd49188e68c6b771
acbeljaars <acbeljaars@gmail.com>
2022-07-20 22:44:04 +0200

    Added api controller

---
+++ src/main/java/com/belj/lil/learningspring/webservice/WebServiceController.java
@@ -0,0 +1,30 @@
+package com.belj.lil.learningspring.webservice;
+
+import com.belj.lil.learningspring.business.ReservationService;
+import com.belj.lil.learningspring.business.RoomReservation;
+import com.belj.lil.learningspring.util.DateUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class WebServiceController{
+    private final DateUtils dateUtils;
+    private final ReservationService reservationService;
+
+    public WebServiceController(DateUtils dateUtils, ReservationService reservationService) {
+        this.dateUtils = dateUtils;
+        this.reservationService = reservationService;
+    }
+
+    @RequestMapping(path="/reservations", method = RequestMethod.GET)
+    public List<RoomReservation> getReservations(@RequestParam(value = "date", required = false)String dateString){
+        Date date = this.dateUtils.createDateFromDateString(dateString);
+        return this.reservationService.getRoomReservationsForDate(date);
+    }
+}
